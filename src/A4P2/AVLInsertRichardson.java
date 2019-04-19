25879c4be4a3b80b00b5c374aa12cddb4be6b628
Jacob Richardson <44219889+JacobRichardson@users.noreply.github.com>
2019-04-18 21:52:37 -0400

    WIP: Implementing AVL Tree.
    This commit creates some of the necessary methods for constructing the AVL Tree.
    Things that still need to be implemented: RL and LR rotations, printTree, and
    finish reading the input file to call insert on the keys.

---
+++ src/A4P2/AVLInsertRichardson.java
@@ -0,0 +1,238 @@
+/* 
+ * A4 P1 AVL Tree
+ *  
+ * Jacob Richardson 
+ * Assignment 4 Part 2
+ * Assigned Date: April 11, 2019 
+ * Due Date: April 22, 2019 
+ * Submission Date: April 22, 2019 
+ */ 
+
+package A4P2;
+
+import java.util.*;
+import java.io.*;
+
+public class AVLInsertRichardson 
+{
+	/*
+	 * This method is the constructor.
+	 * @param fileName This is the string of the name of the input file.
+	 */
+	public AVLInsertRichardson(String fileName)
+	{
+		try {
+			
+			//Create a scanner object for the file.
+			Scanner sc = new Scanner(new File(fileName));
+			
+			//TODO: Use split by \t to get all the keys in an array.
+			
+			PrintWriter w = new PrintWriter("output.txt");
+			
+			
+			
+			//Close the print writer.
+			w.close();
+			
+			//Close the file scanner.
+			sc.close();
+			
+			
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	/*
+	 * This method inserts and AVL node into the tree.
+	 * @param p This is the node that is going to be inserted into the AVL tree.
+	 */
+	public AVLNodeRichardson insert(int k, AVLNodeRichardson p)
+	{
+		//TODO: Figure out how to get h. This means how to tell if the tree has grown in height.
+		
+		//Variable to determine if the tree has grown higher. This is initally set to false.
+		Boolean h = false;
+		
+		AVLNodeRichardson p1;
+		
+		//If p is null.
+		if(p == null) 
+		{
+			
+		}
+		else if( k < p.getKey())
+		{
+			//If the tree has grown on side.
+			if(h)
+			{
+				
+				switch(p.getBalanceFactor())
+				{
+					case 0:
+						//Set the balance factor to one less than what is was before.
+						p.setBalanceFactor(p.getBalanceFactor() - 1);
+					case 1:
+						//Set the bance to zero.
+						p.setBalanceFactor(0);
+						//Set h to false.
+						h = false;
+					case -1:
+						//Set p1 equal to the left node of p.
+						p1 = p.getLeftNode();
+						//If the balance factor of p1 == -1.
+						if(p1.getBalanceFactor() == -1)
+						{
+							//Preform an LL rotation.
+							LLRotation(p);
+							//Set h to false;
+							h = false;
+						}
+						else
+						{
+							//Preform an LR rotation.
+							LRRotation(p);
+							//Set h to false;
+							h = false;
+						}
+						
+				}		
+					
+			}
+		}
+		else
+		{
+			//Set the right node of p equal to the node that is return with k and the right node of p.
+			p.setRightNode(insert(k, p.getRightNode()));
+			
+			//If the tree has grown on side.
+			if(h)
+			{
+				switch(p.getBalanceFactor())
+				{
+					case 0: 
+						//Set the balanceFactor to one more than what it was before.
+						p.setBalanceFactor(p.getBalanceFactor() + 1); 
+					case 1:
+						//Set p1 = to the right node of p.
+						p1 = p.getRightNode();
+						//If the balance factor of p1 == 1
+						if(p1.getBalanceFactor() == 1)
+						{
+							//Preform a RR rotation.
+							RRRotation(p);
+							//Set h to false;
+							h = false;
+						}
+						else
+						{
+							//Preform RL rotation
+							RLRotation(p);
+							//Set h to false;
+							h = false;
+						}
+					case -1:
+						//Set the balance factor of p to 0.
+						p.setBalanceFactor(0);
+						//Set h to false;
+						h = false;
+				}
+			}
+		}
+		
+		
+		//TODO: CHANGE RETURN NULL TO SOMETHHING ELSE.
+		//Return an AVL node.
+		return null;
+		
+	}
+	
+	/*
+	 * This method preforms a left left rotation.
+	 * @param P is the unbalanced node.
+	 */
+	public void LLRotation(AVLNodeRichardson p)
+	{
+		//Set p1 to the left node of p.
+		AVLNodeRichardson p1 = p.getLeftNode();
+		//Set the left node of p to the right node of p1.
+		p.setLeftNode(p1.getRightNode());
+		//Set the right node of p1 to p.
+		p1.setRightNode(p);
+		//Set the balanceFactor of p to 0.
+		p.setBalanceFactor(0);
+		//Set the balanceFactor of p1 to 0.
+		p.setBalanceFactor(0);
+		//Set p equal to p;
+		p = p1;
+	}
+	
+	/*
+	 * This method preforms a left right rotation.
+	 * @param P is the unbalanced node.
+	 */
+	public void LRRotation(AVLNodeRichardson p)
+	{
+		
+		
+	}
+	
+	/*
+	 * This method preforms a right right rotation.
+	 * @param P is the unbalanced node.
+	 */
+	public void RRRotation(AVLNodeRichardson p)
+	{
+		//Set p1 to the right node of p.
+		AVLNodeRichardson p1 = p.getRightNode();
+		//Set the right node of p to the left node of p1.
+		p.setRightNode(p1.getLeftNode());
+		//Set the left node of p1 to p.
+		p1.setLeftNode(p);
+		//Set the balanceFactor of p to 0.
+		p.setBalanceFactor(0);
+		//Set the balanceFactor of p1 to 0.
+		p.setBalanceFactor(0);
+		//Set p equal to p;
+		p = p1;
+	}
+	
+	/*
+	 * This method preforms a right left rotation.
+	 * @param P is the unbalanced node.
+	 */
+	public void RLRotation(AVLNodeRichardson p)
+	{
+		
+	}
+
+	/*
+	 * This method prints out a fully parenthesized expression of the AVL tree.
+	 */
+	public void printTree()
+	{
+		
+	}
+	
+	/*
+	 * This method is the main method of the class.
+	 */
+	public static void main (String args[])
+	{
+		//If args.length does not equal 1.
+		if(args.length != 1)
+		{
+			//Print error message.
+			System.out.println("One argument must be provided.");
+			//Exit with a non zero exit status.
+			System.exit(1);
+		}
+		
+		//Pass the file name to the constructor.
+		new AVLInsertRichardson(args[0]);
+		
+	}
+}
---
+++ src/A4P2/AVLNodeRichardson.java
@@ -0,0 +1,90 @@
+/* 
+ * A4 P1 AVL Tree
+ *  
+ * Jacob Richardson 
+ * Assignment 4 Part 2
+ * Assigned Date: April 11, 2019 
+ * Due Date: April 22, 2019 
+ * Submission Date: April 22, 2019 
+ */ 
+
+package A4P2;
+
+public class AVLNodeRichardson 
+{
+	//Instance Variables.
+	private int balanceFactor;
+	private int key;
+	private AVLNodeRichardson leftNode;
+	private AVLNodeRichardson rightNode;
+	
+	
+	/*
+	 * This is the constructor for the class.
+	 * @param BalanceFactor Integer to represent the nodes balance factor.
+	 */
+	public AVLNodeRichardson(int key, int balanceFactor, AVLNodeRichardson leftNode, AVLNodeRichardson rightNode)
+	{
+		this.key = key;
+		this.balanceFactor = balanceFactor;
+		this.leftNode = leftNode;
+		this.rightNode = rightNode;
+	}
+	
+	/*
+	 * This method returns the key instance variable.
+	 */
+	public int getKey()
+	{
+		return key;
+	}
+	
+	/*
+	 * This method returns the balanceFactor instance variable.
+	 */
+	public int getBalanceFactor()
+	{
+		return balanceFactor;
+	}
+	
+	/*
+	 * This method sets the instance variable balanceFactor to the value passed into the method.
+	 * @param balanceFactor The desired value to set balanceFactor to.
+	 */
+	public void setBalanceFactor(int balanceFactor)
+	{
+		this.balanceFactor = balanceFactor;
+	}
+	
+	/*
+	 * This method returns the leftNode instance variable.
+	 */
+	public AVLNodeRichardson getLeftNode()
+	{
+		return leftNode;
+	}
+	
+	/*
+	 * This method returns the rightNode instance variable.
+	 */
+	public AVLNodeRichardson getRightNode()
+	{
+		return rightNode;
+	}
+	
+	/*
+	 * This method sets the instance variable of the left node to the node passed in.
+	 */
+	public void setLeftNode(AVLNodeRichardson node)
+	{
+		this.leftNode = node;
+	}
+	
+	/*
+	 * This method sets the instance variable of the right node to the node passed in.
+	 */
+	public void setRightNode(AVLNodeRichardson node)
+	{
+		this.rightNode = node;
+	}
+}
